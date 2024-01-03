cf1cb6ba629df2dd489c47edd137d85582d51e7a
pulpydev <pulpy@loucuri.com>
2024-01-04 00:12:19 +0300

    Update soft

--- app/filter.py
+++ app/filter.py
@@ -34,25 +34,28 @@ class Filter:
 
         total_score: float = 0.0
 
-        unique_words_set = set()
-
-        words_in_message: list = message.lower().split() if message else []
-
-        if message.lower() in self.memory:
+        if not message:
             return total_score
 
+       
+        lower_message = message.lower()
+
+        if lower_message in set(self.memory):
+
+            return total_score
+
+        unique_words_set = set()
+
+        words_in_message: list = lower_message.split()
 
         for word, score in self.words.items():
 
             if word.lower() in words_in_message and word.lower() not in unique_words_set:
 
                 total_score += score
-
+                
                 unique_words_set.add(word.lower())
 
-        self.memory.append(message.lower())
-
-        if len(self.memory) > self.memory_limit:
-            self.memory.pop(0)
+        self.memory = [lower_message] + [m for m in self.memory if m != lower_message][:self.memory_limit - 1]
 
         return total_score
